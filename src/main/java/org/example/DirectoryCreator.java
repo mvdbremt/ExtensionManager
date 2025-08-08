e7d3a4f67145a43caac0d1f8f97d0b937c210080
mvdbremt <mathieuvandenbremt@gmail.com>
2025-08-08 17:39:29 +0200

    Added Comments + jar

--- src/main/java/org/example/DirectoryCreator.java
+++ src/main/java/org/example/DirectoryCreator.java
@@ -3,13 +3,24 @@ package org.example;
 import java.io.*;
 public class DirectoryCreator {
     File file;
+
+    /**
+     * Creates a new directory
+     * @param name : Name for new directory
+     *
+     */
     public DirectoryCreator(String name){
+
         file = new File(name);
         file.mkdir();
 
     }
 
+    /**
+     * Moves a file to the directory contained in DirectoryCreator
+     * @param f : File that must be moves in directory
+     */
     public void moveFile(File f){
-        f.renameTo(new File(file.getPath().toString()+"\\"+f.getName()));
+        f.renameTo(new File(file.getPath() +"\\"+f.getName()));
     }
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,30 +1,42 @@
 package org.example;
 
 import java.io.File;
-import java.util.Arrays;
+
 import java.util.HashMap;
 
 public class Main {
     public static void main(String[] args) {
-        String presentFileName="test";
+        String presentFileName="test"; //Default directory name for directory to reorganise
         if(args.length>0){
+            //Get name of directory to organise from arguments if arguments not empty
             presentFileName=args[0];
         }
+
+        //Hashmap containing DirectoryCreator for every extension in directory
         HashMap<String,DirectoryCreator> creators= new HashMap<>();
 
+        //Finds directory to organise
         File here =new File(presentFileName);
+
+        //Manage exception
         if(!here.exists()){
             throw new RuntimeException("No such file : "+presentFileName);
         }
+        //Find list of files in directory
         File[] list= here.listFiles();
+
+        //Main organisation loop
+        assert list != null;
         for (File f : list){
 
             if (f.getName().split("\\.").length<2)continue;
-            String type=f.getName().split("\\.")[f.getName().split("\\.").length-1];
-            if(!creators.keySet().contains(type )){
-                creators.put(type,new DirectoryCreator(presentFileName+"\\"+type));
 
+            String type=f.getName().split("\\.")[f.getName().split("\\.").length-1];
+
+            if(!creators.containsKey(type )){
+                creators.put(type,new DirectoryCreator(presentFileName+"\\"+type));
             }
+
             creators.get(type).moveFile(f);
         }
 
